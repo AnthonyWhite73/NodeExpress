58600e3f84c1e496f0d373cc57a2198d9cf37902
Anthony White <AnthonyRichardWhite@gmail.com>
2021-12-15 11:46:51 +0000

    compare pwds

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -10,3 +10,18 @@ exports.hashPassword = async (req, res, next) => {
         res.status(500).send({message: "Unsuccessful, please try again"})
     }
 };
+
+exports.compare = async (req, res) => {
+    try{
+        console.log(req.body);
+        const user  = await User.findOne({username:req.body.username});
+        console.log(user);
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (!match){
+            res.status(500).send({message: "Did not work"});
+        }
+        res.status(200).send({message: "success"});
+    } catch (error){
+        res.status(500).send({message: "Unsuccessful, please try again"});
+    }
+};
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
-const { addUser, listUsers, updateUser, deleteUser } = require("./userControllers");
-const { hashPassword } = require("../middleware");
+const { addUser, listUsers, updateUser, deleteUser} = require("./userControllers");
+const { hashPassword, compare } = require("../middleware");
 const userRouter = Router();
 
-userRouter.post("/user", hashPassword,addUser);
+// userRouter.delete("/user", deleteUser);
+userRouter.post("/user", hashPassword, addUser);
 userRouter.get("/user", listUsers);
 userRouter.put("/user", updateUser);
 userRouter.delete("/user", deleteUser);
+userRouter.get("/user/checkPassword", compare);
 
 module.exports = userRouter;
