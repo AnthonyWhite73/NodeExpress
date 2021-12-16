da1d5054c8d3a8fed61a5cc756e7717a695209a8
Anthony White <AnthonyRichardWhite@gmail.com>
2021-12-16 16:20:24 +0000

    added email check

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const res = require("express/lib/response");
 const User = require("../user/userModel");
 
 exports.hashPassword = async (req, res, next) => {
@@ -25,3 +26,17 @@ exports.compare = async (req, res) => {
         res.status(500).send({message: "Unsuccessful, please try again"});
     }
 };
+
+exports.checkEmail = async (req, res) => {
+    try {
+        const email = req.body.email;
+        const regex = /.+\@.+\..+/;
+        if (regex.test(email)){
+            res.status(200).send({message: "Valid email, woohoo!"});
+        } else{
+            res.status(500).send({message: "Invalid email, please try again"});
+        }
+    } catch (error){
+        res.status(500).send({message: "Unsuccessful, please try again"});
+    }
+}
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { addUser, listUsers, updateUser, deleteUser} = require("./userControllers");
-const { hashPassword, compare } = require("../middleware");
+const { hashPassword, compare, checkEmail } = require("../middleware");
 const userRouter = Router();
 
 userRouter.post("/user", hashPassword, addUser);
@@ -8,5 +8,6 @@ userRouter.get("/user", listUsers);
 userRouter.put("/user", updateUser);
 userRouter.delete("/user", deleteUser);
 userRouter.get("/user/checkPassword", compare);
+userRouter.get("/user/checkEmail", checkEmail);
 
 module.exports = userRouter;
